9282ea15c8698d214d16469ca537b56335a03520
Pedro <p-cardozo@hotmail.com>
2023-02-15 22:20:01 -0300

    feat(calculadora): adding facotries for PIS, IPI
    refactoring confins calculator

--- src/calculadora/index.ts
+++ src/calculadora/index.ts
@@ -1,10 +1,20 @@
 import { ICMCalculadora } from './caludora-icms'
 import { CofinsCalculadora } from './calculadora-cofins'
+import { PISCalculadora } from './calculadora-pis'
+import { IPICalculadora } from './calculadora-ipi'
 
 export function fazerCalculadoraICMS(percentual: number) {
     return new ICMCalculadora(percentual)
 }
 
-export function fazerCalculadorCofins(percentual: number) {
-    return new CofinsCalculadora(percentual)
+export function fazerCalculadoraCofins() {
+    return new CofinsCalculadora()
+}
+
+export function fazerCalculadoraPIS() {
+    return new PISCalculadora()
+}
+
+export function fazerCalculadoraIPI() {
+    return new IPICalculadora()
 }
