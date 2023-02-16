4229cddedeb4a8102bd5906aa1864d15dcb47a38
Pedro <p-cardozo@hotmail.com>
2023-02-15 22:16:24 -0300

    refactor(interface): orcamento accepts now products not value
    values are composed by products values

--- src/orcamento/interface/orcamento-interface.ts
+++ src/orcamento/interface/orcamento-interface.ts
@@ -1,6 +1,13 @@
 import { Calculadora } from '../../calculadora/interface/calculadora-interface'
 
+export type Produto = {
+    nome: string,
+    valor: number
+}
+
 export interface Orcamento {
     valor: number
+    produtos: Produto[]
+
     calculaImpostos(calculadora: Calculadora[]): number
 }
